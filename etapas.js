af863fae58fcadaa29d63eca4cff655762fb35b2
Aldo Luiz Moura <aldoluiz21@gmail.com>
2021-09-01 00:12:06 -0300

    primeiro commit

---
+++ etapas.js
@@ -0,0 +1,66 @@
+let etapas = [{
+        titulo: 'VEREADOR',
+        numeros: 5,
+        cadidatos: [{
+                numero: 38111,
+                name: 'George Harrison',
+                partido: 'ATMP',
+                fots: [{
+                    url: 'GeorgeHarrison.jpg',
+                    legenda: 'Vereador',
+                    small: false
+                }]
+            },
+            {
+                numero: 77222,
+                name: 'Paul McCartney',
+                partido: 'RAM',
+                fots: [{
+                    url: 'PaulMccartney.jpg',
+                    legenda: 'Vereador',
+                    small: false
+                }]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [{
+                numero: 99,
+                name: 'George Harrison',
+                partido: 'ATMP',
+                Vice: 'Ringo Starr',
+                fotos: [{
+                        url: 'GeorgeHarrison.jpg',
+                        legenda: 'Prefeito'
+
+                    },
+                    {
+                        url: 'ringo.jpg',
+                        legenda: 'Vice-Prefeito'
+                    }
+                ]
+            },
+            {
+                numero: 84,
+                name: 'Paul McCartney',
+                partido: 'RAM',
+                Vice: 'John Lennon',
+                fotos: [{
+                        url: 'PaulMccartney.jpg',
+                        legenda: 'Prefeito',
+                        small: true
+                    },
+                    {
+                        url: 'john.jpg',
+                        legenda: 'Vice-Prefeito',
+                        small: true
+                    }
+                ]
+            }
+
+        ]
+    }
+
+]
